c2ea61afa6ea5551c343835d0a64440f7eb28f53
Agustín Amaya <80552327+bluehat8@users.noreply.github.com>
2024-09-23 03:36:53 -0600

    fix/resolving branch and migration conflicts

---
+++ Migrations/20240923091641_Restore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CredipathAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Restore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "UserPermissions");
+
+            migrationBuilder.DropTable(
+                name: "Routes");
+
+            migrationBuilder.DropTable(
+                name: "Permissions");
+        }
+    }
+}
---
+++ Migrations/20240923092125_restore-keys.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CredipathAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class restorekeys : Migration
+    {
+        /// <inheritdoc />
+            /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Agregar la llave foránea RouteId en Clients
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clients_Routes_RouteId",
+                table: "Clients",
+                column: "RouteId",
+                principalTable: "Routes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull // Permite que Route sea opcional
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Eliminar la llave foránea si se hace rollback
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clients_Routes_RouteId",
+                table: "Clients"
+            );
+        }
+      }
+}
